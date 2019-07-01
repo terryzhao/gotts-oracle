318e3cb37ceeba9c8d088d0ce1ea558c29512e18
terryzhao <terryzhao1981@gmail.com>
2019-07-01 19:29:59 +0800

    add file header license

--- alphavantage/src/lib.rs
+++ alphavantage/src/lib.rs
@@ -1,3 +1,16 @@
+// Copyright 2019 The Gotts Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #![deny(non_upper_case_globals)]
 #![deny(non_camel_case_types)]
@@ -5,13 +18,13 @@
 #![deny(unused_mut)]
 #![warn(missing_docs)]
 
-extern crate failure;
-extern crate failure_derive;
-extern crate serde;
-extern crate serde_json;
-extern crate reqwest;
 extern crate chrono;
 extern crate chrono_tz;
+extern crate failure;
+extern crate failure_derive;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
 
 mod client;
 mod deserialize;
